dfb12083057e367a72d28d18377952f037951d02
LuanCarlosCar <luancarloscar@gmail.com>
2021-03-11 22:24:12 -0300

    destructuring3

---
+++ Fundamentos/destructuring3.js
@@ -0,0 +1,14 @@
+
+              //vou passar um objeto para erra função
+function rand({ min= 0, max=1000}){
+    const valor= Math.random() * (max - min) + min  // vai multiplicar random pelo o resuldado de max - min mais 'min'
+    return Math.floor(valor) //floor ira tirar as casas decimais
+
+}
+console.log(rand({min:10, max:20}))
+
+
+
+const obj = {max:50, min:40}
+console.log(rand(obj))
+console.log(rand({min:955}))
