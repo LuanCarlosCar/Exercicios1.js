5937a39a444cf7b4e6f9bab877a35751b5e107ec
LuanCarlosCar <luancarloscar@gmail.com>
2021-03-12 11:15:02 -0300

    mais exempls

--- Fundamentos/destructuring3.js
+++ Fundamentos/destructuring3.js
@@ -1,7 +1,7 @@
 
-              //vou passar um objeto para erra função
+              //vou passar um objeto para a função
 function rand({ min= 0, max=1000}){
-    const valor= Math.random() * (max - min) + min  // vai multiplicar random pelo o resuldado de max - min mais 'min'
+    const valor= Math.random() * (max - min) + min  // vai multiplicar random pelo o resuldado de (max - min) mais 'min'
     return Math.floor(valor) //floor ira tirar as casas decimais
 
 }
@@ -11,4 +11,4 @@ console.log(rand({min:10, max:20}))
 
 const obj = {max:50, min:40}
 console.log(rand(obj))
-console.log(rand({min:955}))
+console.log(rand({min:10}))
