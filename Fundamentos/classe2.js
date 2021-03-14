58d80b73a81b47ebdeda2bf5da9c14030ca55858
LuanCarlosCar <luancarloscar@gmail.com>
2021-03-14 12:37:03 -0300

    anexando comentarios a class

--- Fundamentos/classe2.js
+++ Fundamentos/classe2.js
@@ -1,12 +1,12 @@
 class Avo{
     constructor(sobrenome){
-        this.sobrenome = sobrenome
+        this.sobrenome = sobrenome //criei a class avo, que tera o atributo 'sobrenome'
     }
 }
 
 class Pai extends Avo{
-    constructor(sobrenome, profissão= 'Developer'){
-        super(sobrenome)
+    constructor(sobrenome, profissão= 'Developer'){ // crei a classe pai que extende a classe avo, ou seja ele recebera sobrenome,e tera como novo atributo profissão
+        super(sobrenome) //'super' ira acessar a super classe(avo), nesse momento estou apentando para o meu atributo sobrenome
         this.profissão=profissão
 
     }
@@ -14,10 +14,10 @@ class Pai extends Avo{
 
 
 class filho extends Pai{
-    constructor(){
-        super('Rodrigues')
+    constructor(){ //criei a classe filho que extende pai
+        super('Rodrigues') // estou acessando a super classe(avo) e nomenado o atributo sobrenome para ('rodrigues')
     }
 
 }
-const t = new filho
-console.log(t)
+const t = new filho //estou criando um novo filho, e colocando ele no eslote de memoria nomeado como 't'
+console.log(t) //estou printando 't' que esta apontando para meu novo filho
